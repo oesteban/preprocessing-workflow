2a481e5df73a87fad2cdd20ec9f1987caff7f32c
oesteban <code@oscaresteban.es>
2016-12-04 08:57:56 -0800

    test is not necessary anymore

--- fmriprep/workflows/fieldmap/base.py
+++ fmriprep/workflows/fieldmap/base.py
@@ -54,7 +54,7 @@ def fmap_estimator(subject_data, settings=None):
         from .phdiff import phdiff_workflow
         phwf = phdiff_workflow(settings)
         # set inputs
-        phdif.inputs.inputnode.input_images = subject_data['fmap']
+        phwf.inputs.inputnode.input_images = subject_data['fmap']
         estimator_wfs.append(phwf)
 
     if any(['fieldmap' in fname for fname in subject_data['fmap']]):
